e55595e4c2a30ec246dc6f703a4d1ba612819dab
zunarvy19 <arvy977#gmail.com>
2024-08-11 17:18:09 +0700

    add sweetalert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,45 @@
-const carouselContainer = document.querySelector(".carousel-container");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+$(document).ready(function () {
+  const $carouselContainer = $(".carousel-container");
+  const $prevButton = $(".prev");
+  const $nextButton = $(".next");
 
-let scrollAmount = 0;
+  let scrollAmount = 0;
+  const cardWidth = $("#card").outerWidth(true);
 
-nextButton.addEventListener("click", () => {
-  carouselContainer.scrollBy({ left: 320, behavior: "smooth" });
-  scrollAmount += 320;
+  function updateScroll(direction) {
+    const containerWidth = $carouselContainer.outerWidth();
+    const scrollPosition = $carouselContainer.scrollLeft();
+
+    if ($(window).width() <= 768) {
+      // Mobile
+      scrollAmount = scrollPosition + direction * cardWidth;
+    } else {
+      // Dekstop
+      scrollAmount = scrollPosition + direction * 320;
+    }
+
+    $carouselContainer.animate({ scrollLeft: scrollAmount }, 400);
+  }
+
+  $nextButton.on("click", function () {
+    updateScroll(1);
+  });
+
+  $prevButton.on("click", function () {
+    updateScroll(-1);
+  });
 });
 
-prevButton.addEventListener("click", () => {
-  carouselContainer.scrollBy({ left: -320, behavior: "smooth" });
-  scrollAmount -= 320;
-});
+// sweetalert
+document
+  .querySelector(".submitForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevents the form from submitting and refreshing the page
+
+    Swal.fire({
+      title: "Terima Kasih!",
+      text: "Anda berhasil mendapatan early access.",
+      icon: "success",
+      confirmButtonText: "Close",
+    });
+  });
